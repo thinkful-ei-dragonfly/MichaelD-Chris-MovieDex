74de1d97dff04c3eace2e652fb972ac3ed9b8c5d
Michael Dusaniwskyj <dusaniwskyj@gmail.com>
2019-06-12 20:36:40 -0400

    changed avg_vote right side of = parseFloat()

--- server.js
+++ server.js
@@ -11,42 +11,39 @@ app.use(morgan('dev'));
 app.use(cors());
 app.use(helmet());
 
-
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN;
   const authToken = req.get('Authorization');
 
-  if(!authToken || authToken.split(' ')[1] !== apiToken) {
-    return res.status(401).json({error: 'Unauthorized request'});
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' });
   }
   //move to the next middleware
   next();
 });
 
-
 app.get('/movie', function handleGetMovie(req, res) {
-  const { genre = '', country = '', avg_vote} = req.query;
+  const { genre = '', country = '', avg_vote } = req.query;
 
-  
   let results = MOVIES;
- if (genre) {
-  results = results.filter(movie => {
-    return movie.genre.toLowerCase().includes(genre.toLowerCase());
-  });
-}
-console.log(results)
-if (country) {
-  results = results.filter(movie => {
-    return movie.country.toLowerCase().includes(country.toLowerCase());
-  });
-}
-  console.log(results)
-if(avg_vote) {
+  if (genre) {
     results = results.filter(movie => {
-      return parseFloat(movie.avg_vote) >= avg_vote;
+      return movie.genre.toLowerCase().includes(genre.toLowerCase());
     });
   }
-console.log(results)
+  console.log(results);
+  if (country) {
+    results = results.filter(movie => {
+      return movie.country.toLowerCase().includes(country.toLowerCase());
+    });
+  }
+  console.log(results);
+  if (avg_vote) {
+    results = results.filter(movie => {
+      return parseFloat(movie.avg_vote) >= parseFloat(avg_vote);
+    });
+  }
+  console.log(results);
   res.json(results);
 });
 
